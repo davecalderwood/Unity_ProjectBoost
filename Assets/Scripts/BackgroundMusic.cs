34d7d4353d83964d97f8e25cb4ed88f333cdec7f
Dave Calderwood <davidhcalderwood@gmail.com>
2021-08-08 12:41:42 -0400

    Background volume slider

--- Assets/Scripts/BackgroundMusic.cs
+++ Assets/Scripts/BackgroundMusic.cs
@@ -1,11 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackgroundMusic : MonoBehaviour
 {
+    [SerializeField] Slider volumeSlider;
     private void Awake() 
     {
         DontDestroyOnLoad(transform.gameObject);
+    }
+
+    private void Start() 
+    {
+        PlayerPrefs.SetFloat("musicVolume", 1);
+        LoadVolume();
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        SaveVolume();
+    }
+
+    private void LoadVolume()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
 }
